053b9876378ad4515815306f9909d77d06d989cc
poornima-whitehatjr <poornima.liam@gmail.com>
2020-05-31 12:37:30 +0530

    Platform

--- sketch.js
+++ sketch.js
@@ -4,8 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg;
-var platform;
+var backgroundImg, platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
